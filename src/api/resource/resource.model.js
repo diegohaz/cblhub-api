648be3d337bc8c5d3f93dc723dc62f30aa342c78
Diego Haz <hazdiego@gmail.com>
2016-10-05 19:07:12 -0300

    Fix resource model

--- src/api/resource/resource.model.js
+++ src/api/resource/resource.model.js
@@ -1,10 +1,10 @@
 import { Schema } from 'mongoose'
 import { Guide } from '../guide'
 
-const questionSchema = new Schema({
+const resourceSchema = new Schema({
   url: String,
   mediaType: String,
   image: String
 })
 
-export default Guide.discriminator('Question', questionSchema)
+export default Guide.discriminator('Resource', resourceSchema)
